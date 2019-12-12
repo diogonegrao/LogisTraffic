b703eaec63bf01b9e85cc8c4e09e3e4c135c0daa
diogonegrao <ruidiogopereira@gmail.com>
2019-12-12 21:49:28 +0000

    Layouts completos (design NÃO final)

--- app/src/main/java/com/example/logistraffic/AdicionarLojaSpinnersActivity.java
+++ app/src/main/java/com/example/logistraffic/AdicionarLojaSpinnersActivity.java
@@ -5,19 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class AdicionarLojaSpinnersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_adicionarlojaspinners);
     }
 
-    @Override
+    public void botaoPesquisa(View v){}
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main_activity, menu);
+        inflater.inflate(R.menu.menu_adicionar_loja, menu);
         return true;
     }
 }
