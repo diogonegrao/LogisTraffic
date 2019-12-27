e5f605d4163e9df2a80e61f64593099b228e0356
diogonegrao <ruidiogopereira@gmail.com>
2019-12-27 15:32:51 +0000

    Cancelar na atividade Sobre

--- app/src/main/java/com/example/logistraffic/SobreActivity.java
+++ app/src/main/java/com/example/logistraffic/SobreActivity.java
@@ -1,10 +1,12 @@
 package com.example.logistraffic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SobreActivity extends AppCompatActivity {
 
@@ -17,6 +19,15 @@ public class SobreActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_sobre, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.cancelar:
+                finish();
+        }
         return true;
     }
 }
